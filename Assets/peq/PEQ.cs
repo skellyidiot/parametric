400f60f609605bf438b3b744b9d43ed97b0d63b7
MUKILTEO\1553223 <aglez0657@gmail.com>
2022-06-06 13:37:06 -0700

    what the fuck

---
+++ Assets/peq/PEQ.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PEQ : MonoBehaviour
+{
+    
+    public static Vector3 Circle(float t, float a = 2)
+    {
+        float x = a * Mathf.Cos(t);
+        float y = a * Mathf.Sin(t);
+        float z = 0;
+
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 Astroid(float t, float a = 3)
+    {
+        float x = a * Mathf.Pow(Mathf.Cos(t), 3);
+        float y = a * Mathf.Pow(Mathf.Sin(t), 3);
+        float z = 0;
+
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 Epicycloid(float t, float a = 1, float b = 5)
+    {
+        float x = (a + b) * Mathf.Cos(t) - b * Mathf.Cos((a / b + 1) * t);
+        float y = (a + b) * Mathf.Sin(t) - b * Mathf.Sin((a / b + 1) * t);
+        float z = 0;
+
+        return new Vector3(x, y, z);
+    }
+}
